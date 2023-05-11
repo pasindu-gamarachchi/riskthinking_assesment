987d80c04d81a4ab37ea2cc0b9b7325bd0dfebeb
pasindu-gamarachchi <pgamarachchi@gmail.com>
2023-05-11 16:56:33 -0400

    Code clean up and docstrings

--- airflow_docker/dags/extraction/data_extraction.py
+++ airflow_docker/dags/extraction/data_extraction.py
@@ -7,40 +7,80 @@ from datetime import datetime
 
 
 class DataExtraction:
+    """
+    A class for performing data extraction operations.
 
-    def __init__(self):
-        pass
+    Methods:
+    - run(): Runs the data extraction process.
+    - get_timestamp(): Retrieves the current timestamp in a formatted string.
+    - get_csv_from_url(): Retrieves a CSV file from a specified URL and saves it locally.
+    - unzip_file(file_path, extract_path): Unzips a specified ZIP file to the specified extraction path.
+    - __create_data_directory(directory_name): Creates a directory to store ZIP files.
+
+    Note:
+    - The class assumes the presence of required libraries such as `os`, `datetime`, `logging`, `zipfile`, and `requests`.
+    - Environment variables are used to configure various parameters, such as data directory, ZIP file name, and CSV file URL.
+    """
 
     def run(self):
-        logging.info(f"Running data extraction.")
+        """
+        Runs the data extraction process.
+
+        This function performs the following steps:
+        1. Retrieves the data directory path from the environment variable "DATA_DIR". If not found, uses the default value "data".
+        2. Retrieves the ZIP file name from the environment variable "ZIP_FILE_NAME". If not found, uses the default value "stock_data.zip".
+        3. Creates the data directory if it does not exist.
+        4. Retrieves the CSV file from a URL.
+        5. Unzips the downloaded ZIP file and extracts its contents to the data directory.
+
+        Note:
+        - The CSV file will be downloaded from a URL specified in the implementation of `get_csv_from_url` method.
+        - The ZIP file will be extracted to the data directory specified in the environment variable "DATA_DIR" or the default value.
+
+        """
+        logging.info("Running data extraction.")
         csv_data_dir = os.getenv("DATA_DIR", "data")
         zipfile_name = os.getenv("ZIP_FILE_NAME", "stock_data.zip")
         self.__create_data_directory(csv_data_dir)
-        # self.temp()
         self.get_csv_from_url()
         self.unzip_file(f"{csv_data_dir}/{zipfile_name}", f"{csv_data_dir}")
 
-    def temp(self):
-        logging.info(f"Current directory : {os.getcwd()}")
-        logging.info(f"List Directory : {os.listdir()}")
-        resp = requests.get('https://www.google.com/')
-        logging.info(f"Status code for google.com : {resp.status_code}")
-
     def get_timestamp(self):
+        """
+        Retrieves the current timestamp in a formatted string.
+
+        Returns:
+        - A formatted timestamp string in the format "YYYYMMDDTHHMMSSZ".
+
+        Example:
+        - "20230511T153045Z"
+
+        """
         current_time = datetime.utcnow()
         formatted_time = current_time.strftime("%Y%m%dT%H%M%SZ")
         return formatted_time
 
     def get_csv_from_url(self):
+        """
+        Retrieves a CSV file from a specified URL and saves it locally.
+
+        Important Steps:
+        1. Retrieve data directory path, ZIP file name, and output file path from environment variables.
+        2. Skip download if ZIP file already exists in the data directory.
+        3. Initiate the download of the CSV file from the URL.
+        4. Write the downloaded file to the output file path.
+
+        Note:
+        - The CSV file is downloaded from a specified URL.
+        - Data directory, ZIP file name, and output file path can be configured using environment variables.
+        - The output file will be overwritten if it already exists.
+        """
         logging.info("get csv from url called")
         csv_data_dir = os.getenv("DATA_DIR", "data")
         zipfile_name = os.getenv("ZIP_FILE_NAME", "stock_data.zip")
         output_file = os.getenv("OUTPUT_FILE_PATH", f"{csv_data_dir}/{zipfile_name}")
-        # CSV_URL = 'https://storage.googleapis.com/kaggle-data-sets/541298/1054465/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com@kaggle-161607.iam.gserviceaccount.com/20230505/auto/storage/goog4_request&X-Goog-Date=20230505T200133Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=74b3d7b85d780828d0fa2a2c5f9da96b72394d28ae517782a5806ffc8c45eb28ce5e5089ecbd4433c363ffad32db26c026febf3f2898a51786b0971d8025c0ccdc49993d37a06bb7b592f30de76ba077d2cc501ab47c6389149da4ed3ecc354e623b89a73aac17cbb3c0a1446f7fc10899858a136a9734c82dbe87df8ac28790afaf461379c8846c0450e94c3e42072c13da513cd40acbb4834ce0bbbef124eb6a306ada6a2f82b00e6e32ee164eed8c2775110fa89cb3f6161aed6f1661109eab636bcdb595814108dfe199762a7a741d0e151bf620b0b1c44dfdff0e151e637c131fb0f160c9b978aa201f0fac3d5007fe685fcfb744397335924eaf3829d1'
-        # CSV_URL = 'https://storage.googleapis.com/kaggle-data-sets/541298/1054465/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20230511%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20230511T162746Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=2118dfd23e6c04b7b072234588133a99f26872cca976f55d98d6973d5a08bd615040c02dddb490d8a722481cebe0ad029869a58fff941c47d5e2f7501c65577aca6690cd4e3e77d591b78008422cd07d357429a83a57e8967efbc6e58169e6c610c064504e70438599c842909c61901ea989a6b5e6b6542a5d4c8c2716fe375e4b112a891a534f44f4af8d2df3d9b519847bb34a37776e5e36bf0cccaec620ae3a01ee2910d1fa2d1c05122024d3d733fd48e9d9def00104f52faa6402ea530027eb88495538aab424927a3a76fbd919cf7f9451d58844ea7cbcf67b7174e013575191e0f475986e4d57544792eec169c0f20ac27353febb6ad1bd6ffa0b274e'
         time_stamp = self.get_timestamp()
-        CSV_URL = f'https://storage.googleapis.com/kaggle-data-sets/541298/1054465/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20230511%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date={time_stamp}&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=a9ed0063f0c4dbdce90217dea4ba8df0d28cc8fe5fe92fdbf3ab866a8b986b2647a6e8abb2b04dfa09f0a4d469d88fd17b20784d327aefb0d1ac4cff6deb9b93fe7be45c6027062513b0a8a0da3c851021ec2d0371e7afa01a9bd2e830e49d44d5cd602a75805df0b73a2b1b9f4aeb7c381132bc5d0c3457c293bb8914172b70619c8ab3864e2ade5f3ce33d1e14d33cc67494b73445f683414a5c287f837b73efcc1abc41fa43198b4c4d2101aea8bab2d30c9eee210b3bd5ce894dbde2add87dba1c387c173ce15f7899a2b27df6536fc4f8eade1ff287e62178d8cced2d476e20c5368ed659b97de82cf4060d33fb6d427f1274e5627f27b2cc934b9e087a'
-        CSV_URL = 'https://storage.googleapis.com/kaggle-data-sets/541298/1054465/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20230511%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20230511T162746Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=2118dfd23e6c04b7b072234588133a99f26872cca976f55d98d6973d5a08bd615040c02dddb490d8a722481cebe0ad029869a58fff941c47d5e2f7501c65577aca6690cd4e3e77d591b78008422cd07d357429a83a57e8967efbc6e58169e6c610c064504e70438599c842909c61901ea989a6b5e6b6542a5d4c8c2716fe375e4b112a891a534f44f4af8d2df3d9b519847bb34a37776e5e36bf0cccaec620ae3a01ee2910d1fa2d1c05122024d3d733fd48e9d9def00104f52faa6402ea530027eb88495538aab424927a3a76fbd919cf7f9451d58844ea7cbcf67b7174e013575191e0f475986e4d57544792eec169c0f20ac27353febb6ad1bd6ffa0b274e'
+        CSV_URL = f'https://storage.googleapis.com/kaggle-data-sets/541298/1054465/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20230511%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date={time_stamp}&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=2118dfd23e6c04b7b072234588133a99f26872cca976f55d98d6973d5a08bd615040c02dddb490d8a722481cebe0ad029869a58fff941c47d5e2f7501c65577aca6690cd4e3e77d591b78008422cd07d357429a83a57e8967efbc6e58169e6c610c064504e70438599c842909c61901ea989a6b5e6b6542a5d4c8c2716fe375e4b112a891a534f44f4af8d2df3d9b519847bb34a37776e5e36bf0cccaec620ae3a01ee2910d1fa2d1c05122024d3d733fd48e9d9def00104f52faa6402ea530027eb88495538aab424927a3a76fbd919cf7f9451d58844ea7cbcf67b7174e013575191e0f475986e4d57544792eec169c0f20ac27353febb6ad1bd6ffa0b274e'
 
         if zipfile_name in os.listdir(csv_data_dir):
             logging.info("File has already been downloaded, skipping download.")
@@ -59,6 +99,21 @@ class DataExtraction:
                             logging.info(f"Written chunk : {chunks_written}")
 
     def unzip_file(self, file_path, extract_path):
+        """
+        Unzips a specified ZIP file to the specified extraction path.
+
+        Args:
+        - file_path (str): The path to the ZIP file to be extracted.
+        - extract_path (str): The path where the contents of the ZIP file will be extracted to.
+
+        The function checks if the expected files are already present in the extraction path. If all the expected files are found, the extraction process is skipped. Otherwise, the function extracts the contents of the ZIP file to the extraction path and logs a message indicating successful extraction.
+
+        Note:
+        - The expected files are checked based on their names (case-sensitive) within the extraction path.
+        - If any of the expected files are missing, the function will extract all contents of the ZIP file.
+        - The function assumes that the ZIP file exists and is accessible.
+
+        """
         expected_files = os.getenv("EXPECTED_FILES", "etfs,stocks,symbols_valid_meta.csv")
         if all(file in os.listdir(extract_path) for file in expected_files.split(',')):
             logging.info("Files have been extracted. Skipping extraction.")
--- airflow_docker/dags/feature_eng/feature_eng.py
+++ airflow_docker/dags/feature_eng/feature_eng.py
@@ -5,28 +5,59 @@ from transform.transform import Transform
 
 
 class FeatureEng(Transform):
+    """
+    FeatureEng class for performing feature engineering on stock and ETF data.
+
+    Inherits from the Transform class.
+
+    Attributes:
+        feature_eng_dir (str): The directory name for feature engineering.
+
+    Methods:
+        __init__(): Initializes a FeatureEng object.
+        run(): Executes the feature engineering process.
+        get_file_names(dir, typ): Retrieves file names for feature engineering.
+        __feature_eng(direct, typ): Performs feature engineering on a given file.
+
+    """
 
     def __init__(self):
         super(FeatureEng, self).__init__({'data_types': {}})
         self.feature_eng_dir = 'feature_eng'
+
     def run(self):
-        logging.info(f"Running Feature Engineering.")
+        """
+        Executes the feature engineering process.
+
+        """
+        logging.info("Running Feature Engineering.")
         for filename in self.get_file_names(f"{self.data_dir}/{self.parquet_files_dir}/{self.stock_data_dir}", 'stock'):
             logging.info(f"Processing file : {filename}.")
-            # self.__load_stocks(f"{self.data_dir}/stocks/{filename}")
             self.__feature_eng(f"{self.data_dir}/{self.parquet_files_dir}/{self.stock_data_dir}/{filename}", "stock")
-            # break
-        logging.info(f"Processing etf files.")
+        logging.info("Processing etf files.")
         for filename in self.get_file_names(f"{self.data_dir}/{self.parquet_files_dir}/{self.etf_data_dir}", 'etf'):
             logging.info(f"Processing file : {filename}.")
             self.__feature_eng(f"{self.data_dir}/{self.parquet_files_dir}/{self.etf_data_dir}/{filename}", 'etf')
-            # break
 
     def get_file_names(self, dir, typ):
+        """
+        Retrieves file names for feature engineering.
+
+        Args:
+            dir (str): The directory to search for files.
+            typ (str): The type of files to retrieve ('stock' or 'etf').
+
+        Yields:
+            str: The file names for feature engineering.
+
+        """
         if not self.is_parq_direct_created:
-            self.create_sub_directory(f"{self.data_dir}/{self.parquet_files_dir}/{self.feature_eng_dir}/{self.stock_data_dir}")
-            self.create_sub_directory(f"{self.data_dir}/{self.parquet_files_dir}/{self.feature_eng_dir}/{self.etf_data_dir}")
+            self.create_sub_directory(f"{self.data_dir}/{self.parquet_files_dir}/"
+                                      f"{self.feature_eng_dir}/{self.stock_data_dir}")
+            self.create_sub_directory(f"{self.data_dir}/{self.parquet_files_dir}/"
+                                      f"{self.feature_eng_dir}/{self.etf_data_dir}")
             self.is_parq_direct_created = True
+
         self.get_parq_file_names(typ, level='feature_eng', feature_eng_dir=self.feature_eng_dir)
         if typ == 'stock':
             for stockfile in os.listdir(dir):
@@ -39,10 +70,16 @@ class FeatureEng(Transform):
                     logging.info(f"{etffile.split('.')[0]} not in {self.parq_etf_file_names}")
                     yield etffile
 
+    def __feature_eng(self, direct, typ):
+        """
+        Performs feature engineering on a given file.
 
-    def __feature_eng(self, dir, typ):
+        Args:
+            direct (str): The path to the file for feature engineering.
+            typ (str): The type of the file ('stock' or 'etf').
 
-        df = pd.read_parquet(dir)
+        """
+        df = pd.read_parquet(direct)
         df.sort_values(by=['Date'], ascending=[True], inplace=True)
         try:
             df['vol_moving_avg'] = df['Volume'].rolling(window=30).mean()
@@ -54,12 +91,14 @@ class FeatureEng(Transform):
         except Exception as err:
             logging.error(f"Failed to add rolling median with {err}.")
 
-        filename = dir.split('/')[-1]
+        filename = direct.split('/')[-1]
         stocksymb = filename.split('.')[0]
 
         if typ == 'stock':
-            parq_output = f"{self.data_dir}/{self.parquet_files_dir}/{self.feature_eng_dir}/{self.stock_data_dir}/{stocksymb}.parquet.gzip"
+            parq_output = f"{self.data_dir}/{self.parquet_files_dir}/{self.feature_eng_dir}/" \
+                          f"{self.stock_data_dir}/{stocksymb}.parquet.gzip"
         elif typ == 'etf':
-            parq_output = f"{self.data_dir}/{self.parquet_files_dir}/{self.feature_eng_dir}/{self.etf_data_dir}/{stocksymb}.parquet.gzip"
+            parq_output = f"{self.data_dir}/{self.parquet_files_dir}/{self.feature_eng_dir}/" \
+                          f"{self.etf_data_dir}/{stocksymb}.parquet.gzip"
 
         df.to_parquet(parq_output)
